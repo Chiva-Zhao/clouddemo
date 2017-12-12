46258e409d9f6718f91c519be104d1fddc6238a4
zhaozh <zhaozhenhua88@gmail.com>
2017-12-13 00:29:58 +0800

    hystrix with ribbon

---
+++ movie-consumer-ribbon-hystrix/src/main/java/com/zzh/cloud/MovieConsumerRibbonHystrixApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieConsumerRibbonHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieConsumerRibbonHystrixApplication.class, args);
+	}
+}
