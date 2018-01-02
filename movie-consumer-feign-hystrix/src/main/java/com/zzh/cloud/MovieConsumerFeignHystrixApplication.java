53d25669db52174c0c978f5ab09025807eecd5d0
jiucifang <zhaozhenhua@jusfoun.com>
2018-01-02 18:17:31 +0800

    Feign and hystrix

---
+++ movie-consumer-feign-hystrix/src/main/java/com/zzh/cloud/MovieConsumerFeignHystrixApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieConsumerFeignHystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieConsumerFeignHystrixApplication.class, args);
+	}
+}
