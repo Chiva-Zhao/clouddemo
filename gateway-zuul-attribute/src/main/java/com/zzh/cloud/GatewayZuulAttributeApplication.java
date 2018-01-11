30379f3eb2fb87c0b0dce79910b1193e841eb0d9
jiucifang <zhaozhenhua@jusfoun.com>
2018-01-12 00:08:07 +0800

    zuul attribute test cases

---
+++ gateway-zuul-attribute/src/main/java/com/zzh/cloud/GatewayZuulAttributeApplication.java
@@ -0,0 +1,13 @@
+package com.zzh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayZuulAttributeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayZuulAttributeApplication.class, args);
+        System.out.println("【【【【【【 GatewayZuulAttribute微服务 】】】】】】已启动.");
+    }
+}
