c1603d08007d0bfda55d3f0167690c23d85ed37f
jiucifang <zhaozhenhua@jusfoun.com>
2018-01-20 00:07:25 +0800

    config server

---
+++ config-server/src/main/java/com/zzh/cloud/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.zzh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * * 配置服务的路劲规则：
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
