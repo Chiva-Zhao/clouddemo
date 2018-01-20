c5836166351e2b5817db51280b862a13ac554ca9
jiucifang <zhaozhenhua@jusfoun.com>
2018-01-20 12:50:45 +0800

    config client

---
+++ config-server-encrypt/src/main/java/com/zzh/cloud/ConfigServerEncryptApplication.java
@@ -0,0 +1,14 @@
+package com.zzh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerEncryptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerEncryptApplication.class, args);
+    }
+}
