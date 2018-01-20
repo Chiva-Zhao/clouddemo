af89e6e3d14229f8b5486d218ae98928c2147b9d
jiucifang <zhaozhenhua@jusfoun.com>
2018-01-20 23:22:28 +0800

    config client refresh

---
+++ config-client-refresh/src/main/java/com/zzh/cloud/ConfigClientRefreshApplication.java
@@ -0,0 +1,12 @@
+package com.zzh.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientRefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientRefreshApplication.class, args);
+	}
+}
