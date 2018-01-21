27814b04bfa63cc84ba3ae5a6cedb3a46bc2a214
jiucifang <zhaozhenhua@jusfoun.com>
2018-01-22 00:54:26 +0800

    simple-aop-log

---
+++ simple-aop-log/src/main/java/com/zzh/boot/controller/AopWebLogController.java
@@ -0,0 +1,20 @@
+package com.zzh.boot.controller;
+
+import com.zzh.boot.entity.User;
+import com.zzh.boot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AopWebLogController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/simple/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userRepository.findOne(id);
+    }
+}
---
+++ simple-aop-log/src/main/java/com/zzh/boot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.zzh.boot.repository;
+
+import com.zzh.boot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
